21dfefd9e12114f47e6c4dbedc4694d47084865e
Kirbosh <66717070+Kirbosh@users.noreply.github.com>
2026-09-22 01:44:13 +0300

    Rebuild as Kirbosh's Paperback 0.8 repository

--- src/Projectsuki/model.ts
+++
@@ -1,4 +0,0 @@
-export interface ProjectsukiMetadata {
-    page?: number;
-    collectedIds?: string[];
-}
--- src/Rawkuma/model.ts
+++
@@ -1,4 +0,0 @@
-export interface RawkumaMetadata {
-  page?: number;
-  collectedIds?: string[];
-}
